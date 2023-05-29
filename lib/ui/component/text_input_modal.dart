d26565ea1189125de4ef7bb9ba1cdd45c5adf28a
kolbe <water.northground@gmail.com>
2023-05-29 22:42:14 +0900

    add dialog and flavor text

--- lib/ui/component/text_input_modal.dart
+++ lib/ui/component/text_input_modal.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+// Flutter imports:
+import 'package:flutter/cupertino.dart';
+
 class TextInputModal extends ConsumerWidget {
   const TextInputModal({super.key});
 
@@ -8,4 +11,32 @@ class TextInputModal extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Container();
   }
+}
+
+Future<bool?> baseShowDialog({
+  required BuildContext context,
+  required String title,
+  GlobalKey<FormState>? formKey,
+  Widget? widget,
+  bool isSimpleDialog = false,
+  bool isOnlyClose = false,
+}) async {
+  final isConfirm = await showDialog<bool>(
+    barrierDismissible: false,
+    context: context,
+    builder: (context) {
+      return CupertinoAlertDialog(
+        title: Text(title),
+        content: widget,
+        actions: [
+          CupertinoDialogAction(
+            isDestructiveAction: true,
+            onPressed: () => Navigator.pop(context, false),
+            child: Text('test'),
+          ),
+        ],
+      );
+    },
+  );
+  return isConfirm;
 }
